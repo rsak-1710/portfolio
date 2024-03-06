6e88f1d22916d65da9b6936f4ebc1d24ad4d391e
rsak-1710 <108388464+rsak-1710@users.noreply.github.com>
2024-03-07 00:37:28 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Profile from "../public/assets/Profile.jpg";
+import Profile from "../public/assets/profile.jpg";
 
 const About = () => {
   return (
